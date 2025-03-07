507b2f66c82955c387417c940e6751402706317a
Timur Ismagilov <bouncepaw2@ya.ru>
2025-03-07 20:56:33 +0300

    Tag 1.4.0-rc1

--- web/handlers.go
+++ web/handlers.go
@@ -498,7 +498,7 @@ func getNodeInfo(w http.ResponseWriter, rq *http.Request) {
 		"version": "2.0",
 		"software": map[string]string{
 			"name":    "betula",
-			"version": "1.3.1",
+			"version": "1.4.0-rc1",
 		},
 		"protocols": []string{"activitypub"},
 		"services": map[string][]string{
