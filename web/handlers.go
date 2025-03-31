470dff8114c7d81f37492f92b13da0b1114eff8c
Timur Ismagilov <bouncepaw2@ya.ru>
2025-03-31 21:52:13 +0300

    Replace v1.4.0-rc1 -> v1.4.0

--- web/handlers.go
+++ web/handlers.go
@@ -499,7 +499,7 @@ func getNodeInfo(w http.ResponseWriter, rq *http.Request) {
 		"version": "2.0",
 		"software": map[string]string{
 			"name":    "betula",
-			"version": "1.4.0-rc1",
+			"version": "1.4.0",
 		},
 		"protocols": []string{"activitypub"},
 		"services": map[string][]string{
