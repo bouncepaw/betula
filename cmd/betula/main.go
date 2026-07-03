15d36114f455649150522636882b54f6747d974b
Timur Ismagilov <bouncepaw2@ya.ru>
2026-07-03 04:19:46 +0300

    Bump version to v1.8.1

--- cmd/betula/main.go
+++ cmd/betula/main.go
@@ -54,7 +54,7 @@ func main() {
 	flag.Parse()
 
 	if versionFlag {
-		fmt.Printf("Betula %s\n", "v1.8.0")
+		fmt.Printf("Betula %s\n", "v1.8.1-wip")
 		return
 	}
 
@@ -104,7 +104,7 @@ func newController() web.Controller {
 		www            = wwwgw.New()
 
 		// One day, all shall be in services!
-		svcSettings  = settingssvc.New(repoSettings, "v1.8.0", settings.SiteDomain)
+		svcSettings  = settingssvc.New(repoSettings, "v1.8.1-wip", settings.SiteDomain)
 		svcNotif     = notifsvc.New(repoNotif)
 		svcArchiving = archivingsvc.New(obeliskFetcher, repoArchives)
 		svcLiking    = likingsvc.New(
--- web/handlers_federated.go
+++ web/handlers_federated.go
@@ -242,7 +242,7 @@ func getNodeInfo(w http.ResponseWriter, rq *http.Request) {
 		"version": "2.0",
 		"software": map[string]string{
 			"name":    "betula",
-			"version": "1.8.0",
+			"version": "1.8.1-wip",
 		},
 		"protocols": []string{"activitypub"},
 		"services": map[string][]string{
